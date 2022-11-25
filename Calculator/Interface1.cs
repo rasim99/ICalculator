cd7181408a542ec59560e4ef718f69accf0d7b64
rasim99 <rasimjm@code.edu.az>
2022-11-25 11:42:27 -0800

    ICaculator

---
+++ Calculator/Interface1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    interface ICaculation
+    {
+        void Calculator();
+       
+    }
+
+    class Calculator : ICaculation
+    {
+        void ICaculation.Calculator()
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a+b);
+            Console.WriteLine(a-b);
+            Console.WriteLine(a*b);
+            Console.WriteLine(a/b);
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICaculation CL = new Calculator();
+            CL.Calculator();
+        }
+    }
+}
